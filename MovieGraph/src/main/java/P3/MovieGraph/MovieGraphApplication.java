2d84fbc932590b0ef4c58d8ebfcf2f7e9b8912b0
Valentín <valenipuente@hotmail.com>
2025-10-22 10:46:43 -0300

    Cree proyecto de Spring Boot

---
+++ MovieGraph/src/main/java/P3/MovieGraph/MovieGraphApplication.java
@@ -0,0 +1,13 @@
+package P3.MovieGraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieGraphApplication.class, args);
+	}
+
+}
---
+++ MovieGraph/src/test/java/P3/MovieGraph/MovieGraphApplicationTests.java
@@ -0,0 +1,13 @@
+package P3.MovieGraph;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieGraphApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
